6bf6809adcb023e8e89f9381709e1a9016bfeb41
PanTongLin <andersonisastupid01@gmail.com>
2019-12-03 14:52:18 +0800

    Update FilterResponseNorm.py

--- FilterResponseNorm.py
+++ FilterResponseNorm.py
@@ -3,11 +3,11 @@ import torch.nn as nn
 
 
 class FilterResponseNorm(nn.Module):
-    def __init__(self, num_features, eps=1e-6, momentum=0.1, use_TLU=True):
+    def __init__(self, num_features, eps=1e-6, use_TLU=True):
         super(FilterResponseNorm, self).__init__()
         self.num_features = num_features
         self.eps = eps
-        self.momentum = momentum
+
         self.use_TLU = use_TLU
 
         self.weight = nn.Parameter(torch.Tensor(num_features))
@@ -17,42 +17,24 @@ class FilterResponseNorm(nn.Module):
         else:
             self.register_parameter('tau', None)
 
-        self.register_buffer('running_var', torch.ones(num_features))
-        self.register_buffer('num_batches_tracked',
-                             torch.tensor(0, dtype=torch.long))
-
         self.reset_parameters()
 
-    def reset_running_stats(self):
-        self.running_var.fill_(1)
-        self.num_batches_tracked.zero_()
-
     def reset_parameters(self):
-        self.reset_running_stats()
         nn.init.uniform_(self.weight)
         nn.init.zeros_(self.bias)
+        if self.use_TLU:
+            nn.init.zeros_(self.tau)
 
     def forward(self, input):
-        if self.momentum is None:
-            exponential_average_factor = 0.0
-        else:
-            exponential_average_factor = self.momentum
 
-        if self.training:
-            self.num_batches_tracked += 1
-            if self.momentum is None:  # use cumulative moving average
-                exponential_average_factor = 1.0 / \
-                    float(self.num_batches_tracked)
-            else:  # use exponential moving average
-                exponential_average_factor = self.momentum
+        nu2 = torch.mean(input.pow(2), (2, 3), keepdim=True)
 
-            nu2 = torch.mean(input.pow(2), (2, 3), keepdim=True)
-            self.running_var = (1-exponential_average_factor) * \
-                self.running_var + exponential_average_factor*nu2
-
-        out = input * torch.rsqrt(self.running_var + abs(self.eps))
+        out = input * torch.rsqrt(nu2 + abs(self.eps))
+        weight = self.weight.unsqueeze(1).unsqueeze(2).expand_as(out)
+        bias = self.bias.unsqueeze(1).unsqueeze(2).expand_as(out)
         # Return after applying the Offset-ReLU non-linearity
         if self.use_TLU:
-            return torch.max(self.weight*out + self.bias, self.tau)
+            tau = self.tau.unsqueeze(1).unsqueeze(2).expand_as(out)
+            return torch.max(weight*out + bias, tau)
         else:
             return self.gamma*out + self.bias
